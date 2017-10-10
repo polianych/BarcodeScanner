a1cb551782ba087c18d57eaf1c4071517ef95ac5
Alexey Poliakov <poliakov.o@gmail.com>
2017-10-10 14:11:03 +0300

    Init ScanFragment logic

--- app/src/main/java/com/example/poliakov/barcodescanner/ScanFragment.java
+++ app/src/main/java/com/example/poliakov/barcodescanner/ScanFragment.java
@@ -1,70 +1,220 @@
 package com.example.poliakov.barcodescanner;
 
+
+import android.Manifest;
 import android.content.Context;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import android.Manifest;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ScanFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ScanFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class ScanFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ScanFragment extends Fragment implements OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-//    private OnFragmentInteractionListener mListener;
 
     public ScanFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScanFragment newInstance(String param1, String param2) {
-        ScanFragment fragment = new ScanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String TAG = "MyActivity";
+    private static final int PERMISSIONS_REQUEST_CAPTURE_IMAGE = 1;
+    CameraSource cameraSource;
+    TextView txtView;
+    SurfaceView cameraView;
+    Detector detector;
+    MediaPlayer mediaPlayer;
+    //    private OnFragmentInteractionListener mListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        super.onCreate(savedInstanceState);
+        final ScanFragment activity = this;
+
+        View RootView = inflater.inflate(R.layout.fragment_scan, container, false);
+
+        Button startBtn = (Button) RootView.findViewById(R.id.start_scan_button);
+        Button stopBtn = (Button) RootView.findViewById(R.id.stop_scan_button);
+        this.txtView = (TextView) RootView.findViewById(R.id.txtContent);
+        this.cameraView = (SurfaceView) RootView.findViewById(R.id.camera_view);
+
+        this.mediaPlayer = new MediaPlayer();
+//        this.mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//            public void onCompletion(MediaPlayer mp) {
+//                activity.mediaPlayer.reset();
+//            }
+//        });
+
+
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // User may have declined earlier, ask Android if we should show him a reason
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CAMERA)) {
+                // show an explanation to the user
+                // Good practise: don't block thread after the user sees the explanation, try again to request the permission.
+            } else {
+                // request the permission.
+                // CALLBACK_NUMBER is a integer constants
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CAPTURE_IMAGE);
+                // The callback method gets the result of the request.
+            }
+        } else {
+            //Init surface and wait user click SCAN button
+            activity.initBarcodeScan();
+        }
+        startBtn.setOnClickListener(this);
+        stopBtn.setOnClickListener(this);
+
+        return RootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        final ScanFragment fragment = this;
+        switch (v.getId()) {
+            case R.id.start_scan_button:
+                Toast.makeText(getActivity(), "Start scan", Toast.LENGTH_SHORT).show();
+                fragment.startBarcodeScan();
+                break;
+            case R.id.stop_scan_button:
+                Toast.makeText(getActivity(), "Stop scan", Toast.LENGTH_SHORT).show();
+                fragment.stopBarcodeScan();
+                break;
+        };
+    }
+
+    public void initBarcodeScan() {
+        final ScanFragment activity = this;
+
+        this.detector =
+                new BarcodeDetector.Builder(getActivity().getApplicationContext())
+                        .setBarcodeFormats(Barcode.ALL_FORMATS)
+                        .build();
+
+        if(!this.detector.isOperational()){
+            this.txtView.setText("Could not set up the detector!");
+            return;
+        }
+        this.cameraSource = new CameraSource
+                .Builder(getActivity().getApplicationContext(), this.detector)
+                .setRequestedPreviewSize(640, 780)
+                .setAutoFocusEnabled(true)
+                .build();
+        this.cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Log.v(TAG, "Surface created!");
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                activity.stopBarcodeScan();
+            }
+        });
+    }
+
+    public void startBarcodeScan() {
+        final ScanFragment activity = this;
+        try {
+            this.detector.setProcessor(new Detector.Processor<Barcode>() {
+                @Override
+                public void release() {
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<Barcode> detections) {
+                    final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                    if (barcodes.size() != 0) {
+                        activity.txtView.post(new Runnable() {    // Use the post method of the TextView
+                            public void run() {
+                                activity.stopBarcodeScan();
+                                activity.processScannedValue(barcodes.valueAt(0).displayValue);
+                                activity.txtView.setText(    // Update the TextView
+                                        barcodes.valueAt(0).displayValue
+                                );
+                            }
+                        });
+                    }
+                }
+            });
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                this.cameraSource.start(this.cameraView.getHolder());
+            }
+        } catch (IOException ie) {
+            Log.e("CAMERA SOURCE", ie.getMessage());
+        }
+    }
+
+    public void stopBarcodeScan() {
+        this.detector.release();
+        this.cameraSource.stop();
+    }
+
+    public void processScannedValue(String value) {
+//        if (value.equals("Hello World!")) {
+//            try{
+//                AssetFileDescriptor afd = getApplicationContext().getAssets().openFd("coin.mp3");
+//                this.mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+//                afd.close();
+//            } catch (IOException ioe) {}
+//
+//        } else {
+//            try{
+//                AssetFileDescriptor afd = getApplicationContext().getAssets().openFd("lost.mp3");
+//                this.mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+//                afd.close();
+//            } catch (IOException ioe) {}
+//
+//        }
+//        try {
+//            this.mediaPlayer.prepare();
+//            this.mediaPlayer.start();
+//        } catch (IOException ioe) {}
+    }
+
+    @Override
+    public void onRequestPermissionsResult ( int requestCode, String[] permissions,
+                                             int[] grantResults){
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAPTURE_IMAGE: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    this.initBarcodeScan();
+                } else {
+                    Log.d("", "permission denied");
+                }
+                return;
+            }
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
